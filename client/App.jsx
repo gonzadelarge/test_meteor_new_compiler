16dadf8cc81f8b57765f7238a6016f4db3b9a0d4
Gonzalo <gcalvo.etraid@grupoetra.com>
2025-06-24 15:23:42 +0200

    first commit

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Hello } from '@client/ui/Hello.jsx';
+import { Info } from '@client/ui/Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    <Hello />
+    <Info />
+  </div>
+);
---
+++ swc.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  jsc: {
+    baseUrl: './',
+    paths: {
+      '@client/*': ['client/*'],
+      '@server/*': ['server/*'],
+      '@common/*': ['common/*'],
+    },
+  },
+};
